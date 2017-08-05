de341c7bec6a08cb840e894ea0688c9a8a9263f3
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-08-05 16:45:25 -0600

    adjust content and styling on music page

--- js/resize-background-index.js
+++ js/resize-background-index.js
@@ -2,5 +2,5 @@ var win = $(window);
 
 var height = win.height();
 
-$(".full-screen").css("height", height)
+$(".full-screen-index").css("height", height)
 $(".intro-container").css("height", 0.7 * height)
---
+++ js/resize-background-music.js
@@ -0,0 +1,6 @@
+var win = $(window);
+
+var height = win.height();
+
+$(".full-screen-music").css("height", height)
+$(".intro-container").css("height", 0.3 * height)
