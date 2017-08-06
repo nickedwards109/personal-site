84ae87ed0c7bafa73f3ba0d3681160625897a78a
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-08-05 18:11:59 -0600

    resize layout containers when browser resizes

--- js/resize-background-index.js
+++ js/resize-background-index.js
@@ -4,3 +4,9 @@ var height = win.height();
 
 $(".full-screen-index").css("height", height)
 $(".intro-container").css("height", 0.7 * height)
+
+$(window).on('resize', function() {
+  var height = win.height();
+	$(".full-screen-index").css("height", height)
+	$(".intro-container").css("height", 0.7 * height)
+});
