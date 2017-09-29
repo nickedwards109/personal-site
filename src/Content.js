98dcd9cdb3ad4919e03d8e09fec2a1c45319f28a
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-28 20:13:31 -0600

    Implement CSS transitions
    When a navbar link is clicked, the new page content fades in.
    
    However, the former page content is not fading out as desired. That is the next step to figure out.

---
+++ src/Content.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import './Content.css';
+
+const Content = (Content) => {
+  return props => (
+    <div>
+      <ReactCSSTransitionGroup
+        transitionName="Fade"
+        transitionAppear={true}
+        transitionAppearTimeout={1000}
+        transitionLeave={true}
+        transitionLeaveTimeout={1000}
+      >
+        <Content />
+      </ReactCSSTransitionGroup>
+    </div>
+  );
+};
+
+export default Content;
--- src/PageContent.js
+++ src/PageContent.js
@@ -3,14 +3,15 @@ import { Route } from 'react-router-dom';
 import About from './About';
 import Music from './Music';
 import Projects from './Projects';
+import Content from './Content';
 
 class PageContent extends Component {
   render() {
     return (
       <div>
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/music" component={Music} />
+        <Route path="/about" component={Content(About)} />
+        <Route path="/projects" component={Content(Projects)} />
+        <Route path="/music" component={Content(Music)} />
       </div>
     );
   }
