99cbf5f24ee15fda72c5ad32a1c5cfcfd915796a
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-10-03 19:09:48 -0600

    Extract setting selected link to a dedicated method
    Previously, the state indicating an active link was set in the JSX, which made it a bit difficult to find the logic for setting state. This commit implements a method setActive(link_name) which sets the active link state when a link is clicked. Keeping this logic in a dedicated method makes it more directly evident where the state is being set.

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,12 @@ import './Navbar.css';
 class Navbar extends Component {
   constructor() {
     super();
-    this.state = { active: "about" }
+    this.state = { active: "about" };
+    this.setActive = this.setActive.bind(this);
+  }
+
+  setActive(link_name) {
+    this.setState({ active: link_name });
   }
 
   Paper(link) {
@@ -15,16 +20,16 @@ class Navbar extends Component {
       <Paper className="navbar-link-container" zDepth={depth}>
         {link}
       </Paper>
-    )
+    );
   }
 
   render() {
     return (
       <div className="navbar-container">
         <div className="gray-line"></div>
-          {this.Paper(<Link to="/about" className="navbar-link" name="about" onClick={() => this.setState({ active: "about"})}>About</Link>)}
-          {this.Paper(<Link to="/projects" className="navbar-link" name="projects" onClick={() => this.setState({ active: "projects"})}>Projects</Link>)}
-          {this.Paper(<Link to="/music" className="navbar-link" name="music" onClick={() => this.setState({ active: "music"})}>Music</Link>)}
+          {this.Paper(<Link to="/about" className="navbar-link" name="about" onClick={() => this.setActive("about")}>About</Link>)}
+          {this.Paper(<Link to="/projects" className="navbar-link" name="projects" onClick={() => this.setActive("projects")}>Projects</Link>)}
+          {this.Paper(<Link to="/music" className="navbar-link" name="music" onClick={() => this.setActive("music")}>Music</Link>)}
           {this.Paper(<a href="https://github.com/nickedwards109" className="navbar-link">Github</a>)}
           {this.Paper(<a href="https://linkedin.com/in/nickedwards109" className="navbar-link">LinkedIn</a>)}
         <div className="gray-line"></div>
