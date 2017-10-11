40fb3e57537812f680ba13e6df1204c7f3bb100c
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-10-11 07:45:38 -0600

    Finish removing Material-UI and update README
    This commit removes the last remaining Material-UI component, and also updates the README with a link to a production deploy.

--- src/Layout.js
+++ src/Layout.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import './Layout.css';
 import { BrowserRouter } from 'react-router-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Navbar from './Navbar';
 import Routes from './Routes';
 
 class Layout extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <div className="Layout">
-          <div className="Layout-header">
-            <h2>Nick Edwards</h2>
-          </div>
-          <BrowserRouter>
-            <div>
-              <Navbar />
-              <Routes />
-            </div>
-          </BrowserRouter>
+      <div className="Layout">
+        <div className="Layout-header">
+          <h2>Nick Edwards</h2>
         </div>
-      </MuiThemeProvider>
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <Routes />
+          </div>
+        </BrowserRouter>
+      </div>
     );
   }
 }
