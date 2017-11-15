027284c2813ee3e80d31a1b213dbbc8ae2cf29b4
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-11-14 18:48:51 -0700

    Change copy on About page and Music page

--- src/Music.js
+++ src/Music.js
@@ -9,9 +9,9 @@ class Music extends Component {
       <div>
         <p>Although I enjoy listening to a broad range of music, most of what I play is Irish Traditional music. I play the Fiddle, Banjo, Guitar, Anglo Concertina, and a few other instruments.</p>
         <p>Irish Traditional music is a huge set of semi-standardized melodies (or "tunes") that have been shared among musicians, dancers, and listeners for many generations. The structure of a traditional tune offers a common framework for multiple musicians to collaborate within. At the same time, the individual musician is free to improvise by spontaneously expressing unique interpretations of notes, chords, timing, and more.</p>
-        <p>Over my 15+ years of playing Irish music, my life has benefited immeasurably from this structure and freedom, community and individuality, and melding of the past and present and future. I aim to share this wonderful music with anyone who would like to listen. So, here is some music I have recorded in hopes that it may benefit others as well.</p>
+        <p>Over my 15+ years of playing Irish music, my life has benefited immeasurably from this structure and freedom, community and individuality, and melding of the past and present and future. I aim to share this wonderful music with anyone who would like to listen. So, here is some music I have recorded for your enjoyment!</p>
         <div className="content-divider"></div>
-        <p>Here is a tune I learned from friends in New Hampshire which is called "The Green Fields of America". I love this tune's aspirational and celebratory mood. This recording features the Fiddle and Anglo Concertina.</p>
+        <p>Here is a traditional Irish tune called "The Green Fields of America". I love this tune's aspirational and celebratory mood. One can imagine the emigrant's hope for a better future, perhaps far across the sea in a distant land. This recording features the Fiddle and the Anglo Concertina.</p>
         <audio controls>
           <source src={greenFieldsOfAmerica} />
         </audio>
