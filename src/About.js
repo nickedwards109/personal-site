5ae14749ed66c26926febc22e98cd88f7fc1eba5
edwardsCNC <nicholas.lee.edwards@gmail.com>
2018-08-06 00:57:04 -0400

    Update copy on About page
    This commit mostly removes copy from the About page, and adds my contact info and a few sentences about my interests.

--- src/About.js
+++ src/About.js
@@ -4,10 +4,9 @@ class About extends Component {
   render () {
     return (
       <div>
-        <p>Hi! My name is Nick. I serve as a Technical Instructor at the Thayer School of Engineering at Dartmouth College, which is where I attended undergraduate and graduate engineering school. I teach problem-solving, machine shop safety, and a broad range of technical skills ranging from introductory manual machining to CAD / CAM Programming / CNC machining. I also support student teams with guidance on diverse issues including fabrication, design for manufacturability, design and implementation of testing strategies, project management, and resolution of teamwork difficulties.</p>
-        <p>My core professional experience involves several years of hands-on work as a senior-level CNC machinist and programmer in a prototype job shop in the aerospace drivetrain manufacturing industry. As a key team member on the shop floor, I contributed to waste elimination efforts by creatively applying Toyota Production System techniques in the areas of machining process design and standardization. I also trained junior machinists in order to foster an elite team capable of addressing difficult challenges in precision machining.</p>
-        <p>Additionally, I have spent time as a machinist in a high-volume automotive manufacturing shop, as an intern at an investment bank, as an intern at a software company, as a student in a software engineering certificate program, and as a ranch hand on a fly-fishing ranch in Colorado! I am also a musician, outdoor enthusiast, and advocate of environmental conservation.</p>
-        <p>My broad experience and team-oriented attitude, coupled with my deep technical focus in machining processes, has enabled me to be a highly valuable mentor and technical resource for budding engineers. I look forward to contributing to positive change in the world while continually supporting the growth of others!</p>
+        <p>Hi! My name is Nick. I currently serve as a Technical Instructor at the Thayer School of Engineering at Dartmouth College, which is where I attended undergraduate and graduate engineering school. I teach problem-solving, machine shop safety, and a broad range of technical skills ranging from introductory manual machining to CAD / CAM Programming / CNC machining. I also support student teams with guidance on diverse issues including fabrication, design for manufacturability, design and implementation of testing strategies, project management, and resolution of teamwork difficulties.</p>
+        <p>I love puzzles. I love challenges. I love music. I love nature. I love people.</p>
+        <p>I would love to hear from you! Drop me a line at nickedwards109@gmail.com anytime.</p>
       </div>
     );
   }
