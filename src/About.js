bd563d613f18588751125dd990d018953aaf8dee
edwardsCNC <nicholas.lee.edwards@gmail.com>
2018-11-26 04:57:40 -0500

    Update About page
    This commit adds content about specific technologies I've used and what I've been learning recently.

--- src/About.js
+++ src/About.js
@@ -4,8 +4,10 @@ class About extends Component {
   render () {
     return (
       <div>
-        <p>Hi! My name is Nick. I currently serve as a Technical Instructor in the Makerspace / Machine Shop at the Thayer School of Engineering at Dartmouth College. I am part of a small team with a mission of teaching. Through hands-on laboratory exercises and creative project support, we support the next generation of engineers as they grow into thoughtful, capable, and empowered professionals.</p>
-        <p>I am highly motivated by the chance to positively impact people's lives and contribute to solving problems in our society!</p>
+        <p>Hi! My name is Nick. I currently work as a Technical Instructor in the Makerspace / Machine Shop at the Thayer School of Engineering at Dartmouth College.</p>
+        <p>Outside of work, I enjoy hiking, mountain biking, and skiing, as well as playing acoustic music on the fiddle, banjo, guitar, and concertina.</p>
+        <p>I also enjoy tinkering with new technologies. Over the past 10 years, I've crafted software in the languages JavaScript, Ruby, MATLAB, HTML, and CSS. I've built full-stack web applications using the Rails MVC framework, and I've built front-end applications with libraries such as jQuery and React. I've designed and implemented relational databases using PostgreSQL, and non-relational databases such as MongoDB. I've built server-side and client-side applications that utilize HTTP as well as WebSockets. In terms of design, I've studied the Material Design philosophy as a guide for thinking about designing user interfaces.</p>
+        <p>Recently, my learning has been focused on deploying applications to production using Linux, AWS EC2, nginx, Puma, DNS, and SSL. I am enjoying learning about production environments, and I look forward to continuously learning about new software engineering tools and techniques in order to solve any problems that I encounter.</p>
         <p>I would love to hear from you. Drop me a line at nickedwards109@gmail.com anytime.</p>
       </div>
     );
