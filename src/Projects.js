ec26c7821c7b3cce56fd0bdd042690d2a074e8fa
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-01-20 14:08:57 -0500

    refactor the logic for processing Projects data
    this commit refactors a long method for generating HTML templates from Projects data. the logic is now separated into distinct methods that each serve a single purpose.

--- src/Projects.js
+++ src/Projects.js
@@ -2,37 +2,63 @@ import React, { Component } from 'react';
 import './Projects.css';
 
 class Projects extends Component {
+  // A project has many children (a title, many paragraphs, and many images).
+  // Aggregate them into a single data structure, sort them in order, and
+  // generate HTML templates to render.
   displayProjects() {
-    // TODO: refactor into single-responsibility methods
     let projects = this.props.data.map(project => {
-      let projectChildren = [];
-      projectChildren.push(project.title)
-      project.paragraphs.forEach(paragraph => {
-        projectChildren.push(paragraph);
-      });
-      project.images.forEach(image => {
-        projectChildren.push(image);
-      });
-      let sortedProjectChildren = projectChildren.sort(
-        function(a, b) {return a.order_index - b.order_index}
-      );
-      let sortedProjectChildrenTemplates = sortedProjectChildren.map(projectChild => {
-         if (projectChild.copy) {
-           // It is a paragraph
-           return <p>{projectChild.copy}</p>
-         }
-         else if (projectChild.url) {
-           // it is an image
-           return <img src={projectChild.url} alt="Project" className="full-size"></img>
-         }
-         else {
-           // It is the title
-           return <h3>{projectChild}</h3>
-         }
-      });
-      return sortedProjectChildrenTemplates;
+      let children = this.generateStackOfChildren(project);
+      let sortedChildren = this.sortChildren(children);
+      let sortedChildrenTemplates = this.generateChildrenTemplates(sortedChildren);
+      return sortedChildrenTemplates;
     });
 
+    return this.generateFinalProjectTemplates(projects);
+  }
+
+  // This method aggregates all of the children in one data structure so that
+  // they can be sorted according to the order they are intended to be displayed in.
+  generateStackOfChildren(project) {
+    let projectChildren = [];
+    projectChildren.push(project.title)
+    project.paragraphs.forEach(paragraph => {
+      projectChildren.push(paragraph);
+    });
+    project.images.forEach(image => {
+      projectChildren.push(image);
+    });
+    return projectChildren;
+  }
+
+  // Each child of a project has an order_index indicating where in the sequence
+  // of children it should be displayed.
+  // This method sorts the children to put them in the correct sequence.
+  sortChildren(children) {
+    return children.sort(
+      function(a, b) {return a.order_index - b.order_index}
+    );
+  }
+
+  // Make an array containing an HTML template for each child
+  generateChildrenTemplates(sortedProjectChildren) {
+    return sortedProjectChildren.map(projectChild => {
+       if (projectChild.copy) {
+         // It is a paragraph
+         return <p>{projectChild.copy}</p>
+       }
+       else if (projectChild.url) {
+         // It is an image
+         return <img src={projectChild.url} alt="Project" className="full-size"></img>
+       }
+       else {
+         // It is the title
+         return <h3>{projectChild}</h3>
+       }
+    });
+  }
+
+  // Make an array containing an HTML template for each project
+  generateFinalProjectTemplates(projects) {
     return projects.map(project =>
       <div className="project-container">
         <div className="project-body">
@@ -40,9 +66,10 @@ class Projects extends Component {
         </div>
         <div className="content-divider"></div>
       </div>
-    )
+    );
   }
 
+  // Render the collection of project templates
   render () {
     return (
       <div>
